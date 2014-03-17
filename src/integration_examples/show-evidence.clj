d7edeff25051765383648c6b55a109332038a12b
Matt Oquist <moquist@majen.net>
2014-03-17 14:41:19 -0400

    change assertion-id to qitem-id

--- src/integration_examples/show-evidence.clj
+++ src/integration_examples/show-evidence.clj
@@ -5,18 +5,18 @@
   (println (json/json-str edn)))
 
 (def user-update
-  {:header {:operation :assert :assertion-id 1234}
+  {:header {:operation :assert :qitem-id 1234}
    :payload [{:entity-type :user
               :entity-id {:user-id 1234}
               :entity {:lastname "Williams"}}]})
 
 (def task-progress
-  {:header {:operation :assert :assertion-id 2345}
+  {:header {:operation :assert :qitem-id 2345}
    :payload [{:entity-id {:enrollment-id 2345 :task-id 3456}
               :entity {:evaluator-id 9876 :status "complete"}}]})
 
 (def task-progress-batch
-  {:header {:operation :assert :assertion-id 3456}
+  {:header {:operation :assert :qitem-id 3456}
    :payload [{:entity-id {:enrollment-id 2345 :task-id 3456}
               :entity {:status "complete"}}
              {:entity-id {:enrollment-id 5678 :task-id 6789}
