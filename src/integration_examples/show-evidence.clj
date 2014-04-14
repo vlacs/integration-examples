45af61dc6aeb052a7d1c0376df4b686c12e8f08a
Matt Oquist <moquist@majen.net>
2014-04-13 23:28:19 -0400

    update queue entry shapes

--- src/integration_examples/show-evidence.clj
+++ src/integration_examples/show-evidence.clj
@@ -5,20 +5,33 @@
   (println (json/json-str edn)))
 
 (def user-update
-  {:header {:operation :assert :qitem-id 1234}
-   :payload [{:entity-type :user
-              :entity-id {:user-id 1234}
-              :entity {:lastname "Williams"}}]})
+  [{:header {:operation :assert
+             :entity-type :user
+             :entity-id {:user-id 1234}}
+    :payload {:entity {:lastname "Williams"}}}])
 
 (def task-progress
-  {:header {:operation :assert :qitem-id 2345}
-   :payload [{:entity-id {:enrollment-id 2345 :task-id 3456}
-              :entity {:evaluator-id 9876 :status "complete"}}]})
+  [{:header {:operation :assert
+             :entity-type :task-progress
+             :entity-id {:enrollment-id 2345 :task-id 3456}}
+     :payload {:entity {:evaluator-id 9876 :status "complete"}}}])
 
 (def task-progress-batch
-  {:header {:operation :assert :qitem-id 3456}
-   :payload [{:entity-id {:enrollment-id 2345 :task-id 3456}
-              :entity {:status "complete"}}
-             {:entity-id {:enrollment-id 5678 :task-id 6789}
-              :entity {:evaluator-id 8765 :status "in-progress"}}]})
+  [{:header {:operation :assert
+             :entity-type :task-progress
+             :entity-id {:enrollment-id 2345 :task-id 3456}}
+    :payload {:entity {:status "complete"}}}
+   {:header {:operation :assert
+             :entity-type :task-progress
+             :entity-id {:enrollment-id 5678 :task-id 6789}}
+    :payload {:entity {:evaluator-id 8765 :status "in-progress"}}}])
+
+
+(def new-task
+  [{:header {:operation :assert
+             :entity-id {:task-id 17}
+             :entity-type :task}
+    :payload {:entity {:name "tie shoes (together)"
+                       :version "v3"
+                       :competency-parents [1 2 3]}}}])
 
